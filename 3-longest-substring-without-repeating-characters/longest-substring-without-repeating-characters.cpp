85bf9383b8b0247f3f8d79694d5b23a17bbeb10a
Shreya Shukla <shreya.shukla2024@vitstudent.ac.in>
2026-09-02 15:45:17 +0530

    Time: 37 ms (43.59%) | Memory: 19.3 MB (29.98%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,13 +5,10 @@ public:
         int n=s.size();
         if (n==0)
         return 0;
-        int i=1;
-        int cnt=0;
-        int ans=1;
-        int left=0;
+        int cnt=0,ans=1,left=0;
         unordered_map<char,int>mpp;
-        mpp[s[0]]=0;
-        while (i<n)
+       
+        for (int i=0;i<n;i++)
         {
             char ch=s[i];
             if (mpp.find(ch)!=mpp.end())
@@ -19,7 +16,7 @@ public:
             mpp[ch]=i;
             cnt=i-left+1;
             ans=max(ans,cnt);
-            i++;
+          
         }
         return ans;
     }
