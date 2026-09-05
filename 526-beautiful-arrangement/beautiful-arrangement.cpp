719df4a0cc441422d944d9a58133b1ccc01dd19a
Shreya Shukla <shreya.shukla2024@vitstudent.ac.in>
2026-09-05 08:24:54 +0530

    Time: 47 ms (45.55%) | Memory: 7.7 MB (94.38%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/beautiful-arrangement.cpp
@@ -1,28 +1,28 @@
-class Solution 
-{
+class Solution {
 public:
-    void calculate(int n,vector<bool>&vis,int i,int & count)
+    void beautiful (int i,int n,int &count,vector<bool>&vis)
     {
         if (i>n)
         {
-            count++;
+            count+=1;
             return;
         }
         for (int j=1;j<=n;j++)
         {
             if (!vis[j]&&(i%j==0||j%i==0))
             {
-            vis[j]=true;
-            calculate(n,vis,i+1,count);
-            vis[j]=false;
+                vis[j]=true;
+                beautiful(i+1,n,count,vis);
+                vis[j]=false;
             }
         }
     }
+
     int countArrangement(int n) 
     {
         int count=0;
         vector<bool>vis(n+1,false);
-        calculate(n,vis,1,count);
+        beautiful(1,n,count,vis);
         return count;
     }
 };
